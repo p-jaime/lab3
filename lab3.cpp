0bcc17579293ee12622e1acd358f1d907e167127
Behruz ashuraliyev <b.ashuraliyev@newuu.uz>
2025-04-07 13:28:04 +0500

    Create lab3.cpp

---
+++ lab3.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+    // Problem 1
+    // int numbers[5];
+    // int sum = 0;
+    // for (int i=0; i<5; i++) {
+    //     cout << i+1 << " number: ";
+    //     cin >> numbers[i];
+    //     sum+=numbers[i];
+    // }
+    // cout << sum << endl;
+
+    // Problem 2
+    // int year, id, fee;
+    // float gpa;
+    //
+    // cout<<"Enter Your ID: ";
+    // cin>>id;
+    // cout<<"Enter Your Year: ";
+    // cin>>year;
+    // cout<<"Enter Your GPA: ";
+    // cin>>gpa;
+    // cout<<"Enter Your Fee: ";
+    // cin>>fee;
+    //
+    // cout<<"Your Student ID " << id << endl;
+    // cout<<"Your GPA " << gpa << endl;
+    // cout<<"Your Year " << year << endl;
+    // cout<<"Your tuition fee " << fee << endl;
+
+
+    // Problem 3
+    // int a;
+    // cout << "Celsius is: ";
+    // cin>>a;
+    // cout<<"Farenheit is: "<<a*1.8+32<<endl;
+
+
+    // Problem 4
+    // float item_cost;
+    // int quantity;
+    // cout << "Enter item cost: ";
+    // cin >> item_cost;
+    // cout << "Enter quantity: ";
+    // cin >> quantity;
+    // cout << "Total cost: $" << item_cost * quantity << endl;
+
+    // Problem 5
+    // cout << "Average age of my family: " << (45+47+19+21+13+13+15)/7 << endl;
+
+    // Problem 6
+    // int length, width;
+    // cout << "Please enter the length and width of your string: ";
+    // cin >> length >> width;
+    // printf("The Perimeter is: %d\n", (length+width)*2);
+    // printf("The Area is: %d\n", length*width);
+
+    // Problem 7
+    // int dividend, divisor;
+    // cout << "Enter dividend: ";
+    // cin >> dividend;
+    // cout << "Enter divisor: ";
+    // cin >> divisor;
+    //
+    // cout << "The quotient: " << dividend/divisor << endl;
+    // cout << "The reminder: " << dividend%divisor << endl;
+
+    // Problem 8
+    // int usd, exchange_rate;
+    // cout << "Please enter in USD: ";
+    // cin >> usd;
+    // cout << "Please enter in exchange rate: ";
+    // cin >> exchange_rate;
+    // cout << usd*exchange_rate << endl;
+
+    // Problem 9
+    // int r;
+    // cout << "Please enter a radious: ";
+    // cin >> r;
+    // cout << "Curface of circle is: " << r*r*3.14 << endl;
+
+    // Problem 10
+    // int height, radius;
+    // cout << "Enter the height of the cylinder: ";
+    // cin >> height;
+    // cout << "Enter the radius of the cylinder: ";
+    // cin >> radius;
+    // float volume = height * radius * radius*3.14;
+    // cout << "Volume is: " << volume << endl;
+
+    // Problem 11
+    // cout << "Velocity in km/h : " << 450/2 << endl;
+    // cout << "Velocity in mile/h : " << 450/1.6/2 << endl;
+
+    // Problem 12
+    // int minutes;
+    // cout << "Enter minutes: ";
+    // cin >> minutes;
+    // cout << minutes/60 << " hours and " << minutes%60 << " minutes" << endl;
+
+    // Problem 13
+    // int cost_per_unit, selling_price_per_unit;
+    // cout << "Enter cost per unit: ";
+    // cin >> cost_per_unit;
+    // cout << "Enter selling price: ";
+    // cin >> selling_price_per_unit;
+    //
+    // cout << "BEP: " << 10000/(selling_price_per_unit-cost_per_unit) <<endl;
+
+    // Problem 14
+    // int basic_salary, tax;
+    // cout << "Please enter your salary: ";
+    // cin >> basic_salary;
+    // cout << "Please enter your tax: ";
+    // cin >> tax;
+    //
+    // cout << "Net salary: " << basic_salary - basic_salary * tax / 100;
+
+    // Problem 15
+    // int original_price, discount_percentage;
+    // cout << "Please enter the price: ";
+    // cin >> original_price;
+    // cout << "Please enter the discount percentage: ";
+    // cin >> discount_percentage;
+    // int discount_amount = original_price * discount_percentage / 100;
+    // cout << "Discount amount: " << discount_amount <<  endl;
+    // cout << "Final price: " << original_price - discount_amount <<  endl;
+
+    // Problem 16
+    // int credit_hours[5];
+    // float grade_points[5];
+    //
+    // for (int i = 0; i < 5; i++) {
+    //     cout << "Enter grade points number " << i+1 << ": ";
+    //     cin >> grade_points[i];
+    //     cout << "Enter credit hours number " << i+1 << ": ";
+    //     cin >> credit_hours[i];
+    //     cout << endl;
+    // }
+    // float upper, lower;
+    // for (int i = 0; i < 5; i++) {
+    //     upper += grade_points[i]*credit_hours[i];
+    //     lower += credit_hours[i];
+    // }
+    // cout << upper << " " << lower << endl;
+    // cout << "The average Weighted GPA: " << upper / lower << endl;
+
+    // Problem 17
+    // int initial_price, final_price;
+    // cout << "Enter initial price: ";
+    // cin >> initial_price;
+    // cout << "Enter final price: ";
+    // cin >> final_price;
+    // float inflation;
+    // inflation = (final_price - initial_price);
+    // inflation = inflation/initial_price*100;
+    // cout << "Inflation: " << inflation << endl;
+
+    // Problem 18
+    // int original_fee, scholarship_rate;
+    // cout << "Please enter your fee: ";
+    // cin >> original_fee;
+    // cout << "Please enter your scholar rate: ";
+    // cin >> scholarship_rate;
+    //
+    // float scholarship_amount = original_fee/100*scholarship_rate;
+    // cout << "Scholarship amount: " << scholarship_amount << endl;
+    // cout << "Final amount: " << original_fee-scholarship_amount << endl;
+
+    // problem 19
+    // int p_amount, rate_of_int, time;
+    // cout << "Enter the amount of time you want to enter: ";
+    // cin >> time;
+    // cout << "Enter rate of interest: ";
+    // cin >> rate_of_int;
+    // cout << "Enter principal amount: ";
+    // cin >> p_amount;
+    // cout << " Simple interest: " << float((p_amount*rate_of_int*time)/100) << endl;
+
+    // Problem 20
+//     int p_amount, rate_of_int;
+//     cout << "Enter annual rate of interest: ";
+//     cin >> rate_of_int;
+//     cout << "Enter principal amount: ";
+//     cin >> p_amount;
+//     float A = (1+float(rate_of_int*0.01))*(1 + float(rate_of_int*0.01))*p_amount;
+//     cout << "Interest after 2 years: " << A-p_amount << endl;
+//     cout << "Total amount after 2 years: " << (A-p_amount)+p_amount << endl;
+// }
